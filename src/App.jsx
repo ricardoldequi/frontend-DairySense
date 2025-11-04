f649d70500435241526d15a8fc91ead555aeaf05
ricardoldequi <ricardo.dequi02@gmail.com>
2025-11-04 09:37:15 -0300

    adicionado rotas para as novas telas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import AppRoutes from './routes/AppRoutes';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Animals from './pages/Animals';
+import Devices from './pages/Devices';
+import DeviceAnimals from './pages/DeviceAnimals';
+import Users from './pages/Users';
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/animals" element={<Animals />} />
+        <Route path="/devices" element={<Devices />} />
+        <Route path="/device-animals" element={<DeviceAnimals />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import Dashboard from '../pages/Dashboard';
 import Animals from '../pages/Animals';
+import Devices from './pages/Devices';
+import Users from '../pages/Users';
+
 
 function AppRoutes() {
   return (
@@ -10,6 +13,10 @@ function AppRoutes() {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/animals" element={<Animals />} />
+        <Route path="/devices" element={<Devices />} />
+        <Route path="/users" element={<Users />} />
+
+
       </Routes>
     </Router>
   );
